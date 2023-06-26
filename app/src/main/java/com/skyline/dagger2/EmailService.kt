8dbf772b06126eae27e58985e266d884ee31b737
Susant Kumar <susant@gmail.com>
2023-06-26 22:44:44 +0530

    Added Dagger Code

--- app/src/main/java/com/skyline/dagger2/EmailService.kt
+++ app/src/main/java/com/skyline/dagger2/EmailService.kt
@@ -5,6 +5,6 @@ import javax.inject.Inject
 
 class EmailService @Inject constructor() {
     fun sentMail(email: String, form: String, topic: String) {
-        Log.d("TAG", "User Saved")
+        Log.d("TAG", "User Saved ")
     }
 }
